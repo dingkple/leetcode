828880ef456ee85ac543b0f383e9caeff661ac58
Zhikai Ding <zding@skillz-14109fe504e1.local>
2015-09-04 11:16:35 -0400

    still running slow

--- leetcode/src/median_of_two_sorted.py
+++ leetcode/src/median_of_two_sorted.py
@@ -10,118 +10,59 @@ class Solution(object):
         :type nums2: List[int]
         :rtype: float
         """
+        if len(nums1) > len(nums2):
+            nums1, nums2 = nums2, nums1
+        l1 = len(nums1)
+        l2 = len(nums2)
 
         while True:
-
-            if len(nums1) > len(nums2):
-                nums1, nums2 = nums2, nums1
-
-            l1 = len(nums1)
-            l2 = len(nums2)
-
             if l1 == 0:
                 return find_mid(nums2)
 
             nums1_mid = nums1[(l1 - 1) / 2]
-            # print l1, 'l1 mid', nums1_mid
 
-            index_of_2 = find_in_array(nums2, nums1_mid)
+            index_of_2 = binary_search_for_index(nums2, nums1_mid)
 
             if l1 == 1:
-                print l1, l2
-                print nums1,
-                print nums2
-                # temp = (l2 - 1) / 2
-                # if l2 % 2 == 0:
-                #     if nums1[0] <= nums2[temp]:
-                #         return nums2[temp]
-                #     elif nums1[0] <= nums2[temp + 1]:
-                #         return nums1[0]
-                #     else:
-                #         return nums2[temp + 1]
-                # else:
-                #     print 'final 1', nums1, nums2
-                #     if l2 == 1:
-                #         return 1.*(nums1[0] + nums2[0]) / 2
-                #     else:
-                #         num2_mid = nums2[l2/2]
-                #         if nums1[0] < num2_mid:
-                #             return 1.*(max([num2_mid-1, nums1[0]]) + num2_mid) / 2
-                #         else:
-                #             return 1.*(min([num2_mid+1, nums1[0]]) + num2_mid) / 2
-
-                insert_index = find_in_array(nums2, nums1[0])
+                insert_index = binary_search_for_index(nums2, nums1[0])
                 if l2 % 2 == 0:
                     return translate(nums1, nums2, insert_index, l2, l2/2)
                 else:
-                    left_index = l2/2
-                    right_index = l2/2+1
-                    left = translate(nums1, nums2, insert_index, l2, left_index)
-                    right = translate(nums1, nums2, insert_index, l2, right_index)
-                    return (left + right) * 1.0 / 2
+                    left = translate(nums1, nums2, insert_index, l2, l2/2)
+                    right = translate(nums1, nums2, insert_index, l2, l2/2+1)
+                    return 1. * (left + right) / 2
 
             if l1 == 2:
-                print nums1, nums2
-                if l2 == 2:
-                    a = max([nums1[0], nums2[0]])
-                    b = min([nums1[1], nums2[1]])
-                    return 1. * (a + b) / 2
-                elif l2 % 2 == 0:
-                    i1 = find_in_array(nums2, nums1[0])
-                    i2 = find_in_array(nums2, nums1[1])
+                i1 = binary_search_for_index(nums2, nums1[0])
+                i2 = binary_search_for_index(nums2, nums1[1])
 
-                    print 'i1', i1, 'i2', i2
-
-                    left_mid = translate(nums1, nums2, i1, i2, (l2+2)/2-1)
-                    right_mid = translate(nums1, nums2, i1, i2, (l2+2)/2)
-
-                    print 'left_mid', left_mid, 'right_mid', right_mid
-
-                    return 1.0 * (left_mid + right_mid) / 2
-
+                if l2 % 2 == 0:
+                    left = translate(nums1, nums2, i1, i2, (l2+2)/2-1)
+                    right = translate(nums1, nums2, i1, i2, (l2+2)/2)
+                    return 1. * (left + right) / 2
                 else:
-                    i1 = find_in_array(nums2, nums1[0])
-                    i2 = find_in_array(nums2, nums1[1])
+                    right = translate(nums1, nums2, i1, i2, (l2+2)/2)
+                    return right
 
-                    print 'i1', i1, 'i2', i2
-
-                    right_mid = translate(nums1, nums2, i1, i2, (l2+2)/2)
-
-                    print 'right_mid', right_mid
-
-                    return right_mid
-
-            print nums1,
-            print nums2,
-            print index_of_2,
-            print real_mid(nums1, nums2)
-
-            if index_of_2 < l2/ 2:
-                t = l1 / 2
-                print t
-                nums1 = nums1[t:]
-                nums2 = nums2[:l2 - t]
-
-            elif index_of_2 == l2 / 2:
-                if l1 % 2 == 0:
-                    t = l1 / 2 - 1
-                    print 't ', t
-                    nums1 = nums1[t:]
-                    nums2 = nums2[:l2 - t]
-                else:
-                    t = l1 / 2
-                    print t
-                    nums1 = nums1[: l1 - t]
-                    nums2 = nums2[t : ]
-
+            if l1%2 == 0:
+                t = l1 / 2 - 1
             else:
                 t = l1 / 2
-                print 't ', t
+
+            if index_of_2 <= l2/ 2:
+                nums1 = nums1[t:]
+                nums2 = nums2[:l2 - t]
+            else:
                 nums1 = nums1[:l1-t]
                 nums2 = nums2[t:]
 
+            l1 -= t
+            l2 -= t
 
-def find_in_array(array, target):
+
+
+
+def binary_search_for_index(array, target):
     left = 0
     right = len(array) - 1
 
@@ -133,33 +74,28 @@ def find_in_array(array, target):
             left = mid + 1
         elif array[mid] > target:
             right = mid - 1
-
-    print target, left
     return left
 
+
 def real_mid(num1, num2):
-    c = []
-    c.extend(num1)
-    c.extend(num2)
-    return find_mid(sorted(c))
+    return find_mid(sorted(num1 + num2))
 
 
-def find_mid(a):
-    len_a = (len(a) - 1) / 2
-    if len(a) % 2 == 0:
-        return 1. * (a[len_a] + a[len_a + 1]) / 2
+def find_mid(array):
+    len_a = (len(array) - 1) / 2
+    if len(array) % 2 == 0:
+        return 1. * (array[len_a] + array[len_a + 1]) / 2
     else:
-        return a[len_a]
+        return array[len_a]
+
 
 def translate(num1, num2, a, b, target):
     if target < a :
-        return num2[a]
+        return num2[target]
     elif target == a:
         return num1[0]
     else:
         target -= 1
-        if a != b:
-            b = b-1
 
     if target < b:
         return num2[target]
@@ -177,18 +113,29 @@ s = Solution()
 a = [1, 2, 4, 45, 64, 57, 56, 865, 785]
 # a = [1, 2, 4, 45, 64, 57, 56, 865]
 # a = [1, 6]
+# a=[100001]
+# a=[1,2]
+# a=[1,3,4]
+# a = [1,2,6,7]
+
 a.sort()
 
 b = [11, 24, 1, 3, 64, 7, 457]
-# b = [11, 24, 1, 3, 64, 7]
+b = [11, 24, 1, 3, 64, 7]
 # b = [4,5,7,10]
+# b=[100000]
+# b=[1,2,3]
+# b=[2,5,6]
+# b=[3,4,5,8]
+
+
 b.sort()
 
 fa = s.findMedianSortedArrays(a, b)
 print 'answer ', fa
 
 print sorted(a)
-print find_in_array(a, 63)
+print binary_search_for_index(a, 63)
 
 c = []
 c.extend(a)
@@ -201,5 +148,5 @@ print find_mid(sorted(c))
 
 d = [4, 45, 56]
 print d
-print find_in_array(d, 11)
-print find_in_array(d, 5)
+print binary_search_for_index(d, 11)
+print binary_search_for_index(d, 5)
