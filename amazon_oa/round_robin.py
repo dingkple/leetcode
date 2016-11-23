ac6ae1493095dacfcc58215620b08c81a145be24
Zhikai Ding <kple@Zhikais-MacBook-Pro.local>
2016-11-22 22:00:59 -0500

    remove the other problem

--- amazon_oa/round_robin.py
+++ amazon_oa/round_robin.py
@@ -1,52 +1,11 @@
-# Definition for a binary tree node.
-class TreeNode(object):
-    def __init__(self, x):
-        self.val = x
-        self.left = None
-        self.right = None
-
-class Solution(object):
-    def kthSmallest(self, root, k):
-        """
-        :type root: TreeNode
-        :type k: int
-        :rtype: int
-        """
-        # 1. not changing the structure
-        nodes = []
-        stack = []
-        node = root
-        while node:
-            stack += node,
-            node = node.left
-            
-        while len(nodes) < k:
-            n = stack.pop()
-            nodes += node,
-            if n.right:
-                t = n.right
-                while t.left:
-                    stack += t.left,
-                    t = t.left
-        return nodes[-1].val
-
-s = Solution()
-
-                
-            
-
-
 def cpu(arr, ela, q):
 
     queue = []
-    cur = 0
 
     queue += (0, 0),
-    total = 0
-    added = 1
-
+    total, added, cur = 0, 1, 0
+    
     while queue:
-
         i, run = queue.pop()
 
         nextcpu = run + q
@@ -71,8 +30,6 @@ def cpu(arr, ela, q):
 
 
 print cpu([0, 1, 4], [5, 2, 3], 3)
-
-# print cpu([])
 
 print cpu([0,1,3,9], [2,1,7,5], 2)
 
